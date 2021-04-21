e50ba48e03562d74411f8d4bc4200d0d37afc5c3
fcoverdugoa <fcoverdugoa@underpost.net>
2021-04-21 00:43:40 -0400

    update path

--- src/api/view.js
+++ src/api/view.js
@@ -1,7 +1,7 @@
 var view = (url, view)=>{
   let data_view = {
     client: fs.readFileSync((path+'/view/'+view+'/core.js'), charset),
-    plaform: fs.readFileSync((path+'/view/'+view+'/src/plaform.js'), charset),
+    platform: fs.readFileSync((path+'/view/'+view+'/src/platform.js'), charset),
     init: fs.readFileSync((path+'/view/'+view+'/src/init.js'), charset),
     param: fs.readFileSync((path+'/view/'+view+'/src/param.js'), charset),
     var: fs.readFileSync((path+'/view/'+view+'/src/var.js'), charset),
@@ -12,7 +12,7 @@ var view = (url, view)=>{
   data_view.client = data_view.client.replace('[[VAR]]',data_view.var);
   data_view.client = data_view.client.replace('[[INIT]]',data_view.init);
   data_view.client = data_view.client.replace('[[RENDER]]',data_view.render);
-  data_view.client = data_view.client.replace('[[PLATFORM]]',data_view.plaform);
+  data_view.client = data_view.client.replace('[[PLATFORM]]',data_view.platform);
   app.get(url, (req, res) => {
     // reduce
     res.send(reduce((`<script type='text/javascript'>`+data_view.client+`</script>`)));
